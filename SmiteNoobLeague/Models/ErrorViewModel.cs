d0f391e8008908d8baeb21528a49d129e9694f70
kevinbevers <kevin.bevers@hotmail.com>
2020-05-11 13:42:13 +0200

    Added codebase again...

---
+++ SmiteNoobLeague/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmiteNoobLeague.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
